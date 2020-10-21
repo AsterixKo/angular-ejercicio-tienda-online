6f3ed1633a21b593c6e1c110af6262a4ee3edf75
MSI\moneo <jvr.moneo@gmail.com>
2020-10-21 14:38:06 +0200

    Crear componentes iniciales y servicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HowComponent } from './core/components/how/how.component';
+import { ProductsComponent } from './core/components/products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'how', component: HowComponent },
+  { path: 'products', component: ProductsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { HowComponent } from './core/components/how/how.component';
+import { ProductsComponent } from './core/components/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HowComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
